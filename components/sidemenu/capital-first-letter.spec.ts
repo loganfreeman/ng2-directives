951b00a65cef91896d4bb7ce5df9ecd566569229
shanhong cheng <shnahongcheng@gmail.com>
2016-06-03 23:28:34 -0600

    tranform

--- components/sidemenu/capital-first-letter.spec.ts
+++ components/sidemenu/capital-first-letter.spec.ts
@@ -1,11 +1,11 @@
 import {CapitalFirstPipe} from './capital-first-letter';
-import { PipeTransform } from 'angular2/core';
 
 describe('capital first letter', () => {
   it('should captial first letter', () => {
     let source:string = 'beijing';
-    let pipe:PipeTransform = new CapitalFirstPipe();
+    let pipe:any = new CapitalFirstPipe();
     let result:string = 'Beijing';
-    expect(pipe.transform(source)).toEqual(result);
+    let actual:string = pipe.transform(source);
+    expect(actual).toEqual(result);
   });
 });
